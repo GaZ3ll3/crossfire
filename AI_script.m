c255f1efca3c337a189fac20b79590e2aa33f01a
Yimin <zhongym8903@gmail.com>
2014-09-05 08:39:11 -0500

    bug

--- AI_script.m
+++ AI_script.m
@@ -2,143 +2,16 @@ function [res, frompos, topos] = AI_script(color, own_pieces, database, h)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-res = 1;
-% disp(color);
-
-
-nothreat = find_threat(h);
-
 if color == 'r'
-
-    moves = avail_move('r', h);
-    
-    if isempty(moves)
-        res = 0;
-        frompos = -1;
-        topos = -1;
-        return;
-    end
-    
-    
-    
-    if (nothreat(1))
-        ind = randi(size(moves,1));
-        frompos = moves(ind, 1);
-        topos = moves(ind, 2);        
-    
-    else 
-        admissible = find(h.piece(25) + 5 == moves(:,2));
-        if isempty(admissible)
-            ind = randi(size(moves,1));
-            frompos = moves(ind, 1);
-            topos = moves(ind, 2);  
-        else
-            frompos = moves(admissible(1), 1);
-            topos = moves(admissible(1), 2);
-        end
-    
-        
-    end
-    
-    
-
+    pstart = 0;
+elseif color == 'b'
+    pstart = 25;
+elseif color == 'g'
+    pstart = 50;
+elseif color == 'd'
+    pstart = 75;
 end
-
-if color == 'b'
-
-    moves = avail_move('b', h);
-    if isempty(moves)
-        res = 0;
-        frompos = -1;
-        topos = -1;
-        return;
-    end
-    
-    
-    if (nothreat(2))
-        ind = randi(size(moves,1));
-        frompos = moves(ind, 1);
-        topos = moves(ind, 2);        
-    
-    else 
-        admissible = find(h.piece(50) + 5== moves(:,2));
-        if isempty(admissible)
-            ind = randi(size(moves,1));
-            frompos = moves(ind, 1);
-            topos = moves(ind, 2);  
-        else
-            frompos = moves(admissible(1), 1);
-            topos = moves(admissible(1), 2);
-        end
-    
-        
-    end
-
-end
-
-if color == 'g'
-
-    moves = avail_move('g', h);
-    if isempty(moves)
-        res = 0;
-        frompos = -1;
-        topos = -1;
-        return;
-    end
-    
-    if (nothreat(3))
-        ind = randi(size(moves,1));
-        frompos = moves(ind, 1);
-        topos = moves(ind, 2);        
-    
-    else 
-        admissible = find(h.piece(75) + 5==moves(:,2));
-        if isempty(admissible)
-            ind = randi(size(moves,1));
-            frompos = moves(ind, 1);
-            topos = moves(ind, 2);  
-        else
-            frompos = moves(admissible(1), 1);
-            topos = moves(admissible(1), 2);
-        end
-    
-        
-    end
-    
-
-end
-
-if color == 'd'
-
-    moves = avail_move('d', h);
-    if isempty(moves)
-        res = 0;
-        frompos = -1;
-        topos = -1;
-        return;
-    end
-    
-    if (nothreat(4))
-        ind = randi(size(moves,1));
-        frompos = moves(ind, 1);
-        topos = moves(ind, 2);        
-    
-    else 
-        admissible = find(h.piece(100) + 5==moves(:,2));
-        if isempty(admissible)
-            ind = randi(size(moves,1));
-            frompos = moves(ind, 1);
-            topos = moves(ind, 2);  
-        else
-            frompos = moves(admissible(1), 1);
-            topos = moves(admissible(1), 2);
-        end
-    
-        
-    end
-
-end
-
+[res, frompos, topos] = rand_ai(color, h, pstart);
 
 end
 
@@ -174,9 +47,11 @@ function [ret] = avail_move(color, h)
         end
         
         for j = 1:size(temp, 2)
-            if temp(j) ~= pos && ~isallies(pos, temp(j), h) && ((iscamp(temp(j)) && ...
-                    h.pos(temp(j)) == -1) || (~iscamp(temp(j)))) && ((isbase(temp(j)) && ...
-                    h.pos(temp(j)) ~= -1) || (~isbase(temp(j))))
+            if temp(j) ~= pos && ~isallies(pos, temp(j), h) && ...
+                    ((iscamp(temp(j)) && h.pos(temp(j)) == -1) || ...
+                    (~iscamp(temp(j)))) && ((isbase(temp(j)) && h.pos(temp(j)) ~= -1) && ...
+                    (h.flag(floor((h.piece(temp(j)) - 1)/25) + 1) ~= 1 ||...
+                    (h.flag(floor((h.piece(temp(j)) - 1)/25)) ==1  && h.pos(temp(j)) == floor((h.piece(temp(j)) - 1)/25)*25 + 25))) || (~isbase(temp(j)))
                 rows = rows + 1;
                 ret(rows, 1) = pos;
                 ret(rows, 2) = temp(j);
@@ -351,22 +226,80 @@ function [ret] = rail_go_from(position, h)
 end
 
 
-function ret = find_threat(h)
-ret = ones(4,1);
-if h.piece(25) ~= -1 && ~isallies(h.piece(25), h.piece(25) + 5, h) 
-    ret(1) = 0;
+function ret = find_top_threat(color, h)
+ret = 1;
+if color == 'r' && h.piece(25) ~= -1 && ~isallies(h.piece(25), h.piece(25) + 5, h)
+    ret = 0;
+    return;
 end
 
-if h.piece(50) ~= -1 && ~isallies(h.piece(50), h.piece(50) + 5, h) 
-    ret(2) = 0;
+if  color == 'b' && h.piece(50) ~= -1 && ~isallies(h.piece(50), h.piece(50) + 5, h)
+    ret = 0;
+    return;
 end
 
-if h.piece(75) ~= -1 && ~isallies(h.piece(75), h.piece(75) + 5, h) 
-    ret(3) = 0;
+if color == 'g' && h.piece(75) ~= -1 && ~isallies(h.piece(75), h.piece(75) + 5, h) 
+    ret = 0;
+    return;
 end
 
-if h.piece(100) ~= -1 && ~isallies(h.piece(100), h.piece(100) + 5, h) 
-    ret(4) = 0;
+if color == 'd'&& h.piece(100) ~= -1 && ~isallies(h.piece(100), h.piece(100) + 5, h) 
+    ret = 0;
+    return;
 end
 
 end
+
+
+
+function [res, frompos, topos] = rand_ai(color ,h, pstart)
+
+    res = 1;
+    
+    moves = avail_move(color, h);
+    
+    
+    if isempty(moves)
+        res = 0;
+        frompos = -1;
+        topos = -1;
+        return;
+    end
+    
+    
+    nothreat = find_top_threat(color, h);
+    
+    if (nothreat)
+        ind = randi(size(moves,1));
+        frompos = moves(ind, 1);
+        topos = moves(ind, 2);        
+    
+    else 
+        admissible = find(h.piece(pstart + 25) + 5 == moves(:,2));
+        if isempty(admissible)
+            ind = randi(size(moves,1));
+            frompos = moves(ind, 1);
+            topos = moves(ind, 2);  
+        else
+            temp = intersect(find(h.pos(moves(admissible, 1)) ~= pstart + 20), find(h.pos(moves(admissible, 1)) ~= pstart + 21));
+            if ~isempty(temp)
+                % not a bomb
+                frompos = moves(h.pos(temp(1)), 1);
+                topos = moves(h.pos(temp(1)), 2);
+                
+            else
+                frompos = moves(admissible(1), 1);
+                topos = moves(admissible(1), 2);
+            end
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
